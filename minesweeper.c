67ee7f7da1ebaa34c14885cd62fe9abd6a13fc60
Patadass <ajdinskiivan1@gmail.com>
2026-04-10 22:05:55 +0200

    added options
    added options and help message
    changed all uint8_t to uint16_t

--- minesweeper.c
+++ minesweeper.c
@@ -2,29 +2,33 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 #include <time.h>
 #include <signal.h>
 
+#include <getopt.h>
+#include <err.h>
+
 #define GAMEOVER 1
 #define WIN 2
 #define OK 0
 
-static uint8_t HEIGHT = 9;
-static uint8_t WIDTH = 9;
-static uint8_t NUM_BOMBS = 10;
-static uint8_t FLAGGED = 0;
+static uint16_t HEIGHT = 9;
+static uint16_t WIDTH = 9;
+static uint16_t NUM_MINES = 10;
+static uint16_t FLAGGED = 0;
 
 typedef struct field{
-    uint8_t n_mines;        // num of mines around filed ( if 9 then is bomb )
+    uint16_t n_mines;        // num of mines around filed ( if 9 then is bomb )
     bool    is_selected;    // true if field has been selected     
     bool    is_flaged;      // ture if field has been flaged
 } field;
 
 typedef struct cursor{
-    uint8_t x;
-    uint8_t y;
+    uint16_t x;
+    uint16_t y;
 } cursor;
 
 //sets color acording to given field, cursor and field postion
@@ -32,8 +36,8 @@ typedef struct cursor{
 //if not selected but hovered by cursor set yellow bg and yellow fg
 //if selected but not hoverd by cursor sets white bg and black fg
 //if selected and hoverd by cursor sets yellow bg and black fg
-void set_color(field f, cursor c, uint8_t x, uint8_t y){
-    uint8_t fg = 255, bg = 255;
+void set_color(field f, cursor c, uint16_t x, uint16_t y){
+    uint16_t fg = 255, bg = 255;
 
     if(f.is_selected || f.is_flaged){
         fg = 0;
@@ -85,12 +89,12 @@ void select_field(field** gameboard, int8_t x, int8_t y){
 }
 
 
-uint8_t run_game_logic(field** gameboard){
-    uint8_t i, j;
-    uint8_t code = OK;
+uint16_t run_game_logic(field** gameboard){
+    uint16_t i, j;
+    uint16_t code = OK;
 
-    uint8_t to_be_selected = 0;
-    uint8_t selected = 0;
+    uint16_t to_be_selected = 0;
+    uint16_t selected = 0;
 
     for(i = 0; i < HEIGHT; i++){
         for(j = 0; j < WIDTH; j++){
@@ -117,8 +121,8 @@ uint8_t run_game_logic(field** gameboard){
 }
 
 // counts the number of mines around gameboard[x][y]
-uint8_t bombs_around_field(field** gameboard, int8_t x, int8_t y){
-    uint8_t n = 0;
+uint16_t bombs_around_field(field** gameboard, int8_t x, int8_t y){
+    uint16_t n = 0;
 
     int8_t i, j;
 
@@ -145,12 +149,12 @@ uint8_t bombs_around_field(field** gameboard, int8_t x, int8_t y){
     return n;
 }
 
-field** init_fileds(uint8_t mines){
+field** init_fileds(uint16_t mines){
 
     field** gameboard = malloc(sizeof(field*) * HEIGHT);
 
     int8_t i, j;
-    uint8_t x, y;
+    uint16_t x, y;
 
     for(i = 0; i < HEIGHT; i++){
         gameboard[i] = malloc(sizeof(field) * WIDTH);
@@ -181,7 +185,7 @@ field** init_fileds(uint8_t mines){
 }
 
 void free_fields(field** gameboard){
-    uint8_t i;
+    uint16_t i;
 
     for(i = 0; i < HEIGHT; i++){
         free(gameboard[i]);
@@ -191,12 +195,12 @@ void free_fields(field** gameboard){
 }
 
 void print_gameboard(field** gameboard, cursor c){
-    uint8_t i, j;
+    uint16_t i, j;
     printf("\033[H"); // move cursor to 0, 0
     
     printf("minesweeper.c\n\n");
 
-    printf("bombs flagged: %u / %u%5s\n\n", FLAGGED, NUM_BOMBS,"");
+    printf("bombs flagged: %u / %u%5s\n\n", FLAGGED, NUM_MINES,"");
 
     for(i = 0; i < HEIGHT; i++){
         for(j = 0; j < WIDTH; j++){
@@ -256,14 +260,96 @@ void signal_callback_handler(int signum){
     exit(signum);
 }
 
-int main(){
+void print_help(int exit_code){
+    errx(exit_code, "minesweeper [--help] [-w <width>] [-h <height>] [-m <num of mines>]\n\npresets:\n"
+            "\tminesweeper --easy (default)\n\theight 9 width 9 mines 10\n\n"
+            "\tminesweeper --hard\n\theight 16 width 16 mines 40\n\n"
+            "\tminesweeper --expert\n\theight 16 width 30 mines 99");
+}
+
+int main(int argc, char** argv){
+
+    int o;
+
+    int option_index = 0;
+    struct option longopts[] = {
+        {"help",    no_argument,       NULL,  0},
+        {"easy",    no_argument,       NULL,  0},
+        {"hard",    no_argument,       NULL,  0},
+        {"expert",  no_argument,       NULL,  0},
+        {"width",   required_argument, NULL, 'w'},
+        {"height",  required_argument, NULL, 'h'},
+        {"mines",   required_argument, NULL, 'm'},
+        {0, 0, 0, 0}
+    };
+    char* optstring = "w:h:m:";
+
+    while((o = getopt_long(argc, argv, optstring, longopts, &option_index)) != -1){
+        int ph;
+        switch(o){
+            case 0:
+                if(strcmp(longopts[option_index].name, "help") == 0){
+                    print_help(EXIT_SUCCESS);
+                }
+                if(strcmp(longopts[option_index].name, "easy") == 0){
+                    HEIGHT = 9;
+                    WIDTH = 9;
+                    NUM_MINES = 10;
+                }
+                if(strcmp(longopts[option_index].name, "hard") == 0){
+                    HEIGHT = 16;
+                    WIDTH = 16;
+                    NUM_MINES = 40;
+                }
+                if(strcmp(longopts[option_index].name, "expert") == 0){
+                    HEIGHT = 16;
+                    WIDTH = 30;
+                    NUM_MINES = 99;
+                }
+                break;
+            case 'w':
+                ph = atoi(optarg);
+                if(ph > 255 || ph < 9){
+                    printf("width must be less then 255 and more then 9\n");
+                    print_help(EXIT_FAILURE);
+                }
+                WIDTH = atoi(optarg);
+            break;
+            case 'h':
+                ph = atoi(optarg);
+                if(ph > 255 || ph < 9){
+                    printf("height must be less then 255 and more then 9\n");
+                    print_help(EXIT_FAILURE);
+                }
+                HEIGHT = atoi(optarg);
+            break;
+            case 'm':
+                ph = atoi(optarg);
+                if(ph > 255 || ph < 9){
+                    printf("number of mines must be less then 255 and more then 9\n");
+                    print_help(EXIT_FAILURE);
+                }
+                NUM_MINES = atoi(optarg);
+            break;
+            default:
+                print_help(EXIT_FAILURE);
+            break;
+        }
+    }
+
+    int ch = HEIGHT, cw = WIDTH, cb = NUM_MINES;
+
+    if(cb > ch * cw){
+        printf("number of mines cant be more then the number of fields");
+        print_help(EXIT_FAILURE);
+    }
 
     printf("\033[?25l\033[2J"); // make cursor invisible and clear screen
 
     signal(SIGINT, signal_callback_handler);
     set_buffered_input(false);
 
-    field** gamebaord = init_fileds(NUM_BOMBS);
+    field** gamebaord = init_fileds(NUM_MINES);
     cursor game_cursor;
     game_cursor.x = 0;
     game_cursor.y = 0;
@@ -273,7 +359,6 @@ int main(){
     while(true){
         
         print_gameboard(gamebaord, game_cursor);
-
 
         c = getchar();
 
@@ -345,7 +430,7 @@ int main(){
             break;
         }
 
-        uint8_t code = run_game_logic(gamebaord);
+        uint16_t code = run_game_logic(gamebaord);
         if(code == GAMEOVER){
             print_gameboard(gamebaord, game_cursor);
             printf("GAME OVER\n");
